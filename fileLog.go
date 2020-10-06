b04bf0e15e532611bccee23cd2bc3014ea833b7f
ZYKJShadow <857405578@qq.com>
2020-10-06 10:40:57 +0800

    支持后台异步日志

--- fileLog.go
+++ fileLog.go
@@ -7,6 +7,13 @@ import (
 	"time"
 )
 
+var (
+	//日志通道最大值
+	MaxSize = 50000
+	//后台线程数
+	threadNums = 1
+)
+
 type FileLogger struct {
 	Level       LogLevel
 	filePath    string //保存路径
@@ -14,6 +21,16 @@ type FileLogger struct {
 	fileObj     *os.File
 	errFileObj  *os.File
 	maxFileSize int64
+	logChan     chan *logMsg
+}
+
+type logMsg struct {
+	Level     LogLevel
+	msg       interface{}
+	funcName  string
+	fileName  string
+	timestamp string
+	line      int
 }
 
 func NewFileLogger(levelStr LogLevel, fp, fn string, maxSize int64) *FileLogger {
@@ -22,6 +39,7 @@ func NewFileLogger(levelStr LogLevel, fp, fn string, maxSize int64) *FileLogger 
 		filePath:    fp,
 		fileName:    fn,
 		maxFileSize: maxSize,
+		logChan:     make(chan *logMsg, MaxSize),
 	}
 	err := fileLogger.initFile()
 	if err != nil {
@@ -36,6 +54,7 @@ func (f *FileLogger) Close() {
 }
 
 func (f *FileLogger) initFile() error {
+
 	fullFileName := path.Join(f.filePath, f.fileName)
 	file, err := os.OpenFile(fullFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
@@ -44,35 +63,69 @@ func (f *FileLogger) initFile() error {
 	}
 
 	errFile, err := os.OpenFile(fullFileName+".err", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
 	if err != nil {
 		fmt.Printf("open err log file failed,err:%v\n", err)
 		return err
 	}
 
+	defer f.Close()
+
 	f.fileObj = file
 	f.errFileObj = errFile
+
+	//开启后台日志
+	for i := 0; i < threadNums; i++ {
+		go f.writeInBack()
+	}
+
 	return nil
 }
 
-func (f *FileLogger) RecordFileLog(printLevel LogLevel, msg ...interface{}) {
-	if f.Level >= printLevel {
-		t := time.Now().Format("2006-01-02 15:04:05")
-		funcName, fileName, lineNo := getInfo(2)
-		isBig, err := checkSize(f.fileObj, f.maxFileSize)
-		if err != nil {
-			fmt.Printf("check size error err:%v\n", err)
-			return
-		}
-		if isBig {
-			f.fileObj = f.splitFile(f.fileObj)
-		}
-		_, _ = fmt.Fprintf(f.fileObj, "[%s] [%s] [%s:%s:%d] %v\n", t, parseLevel(f.Level), funcName, fileName, lineNo, msg)
+func (f *FileLogger) writeInBack() {
+	isBig, err := checkSize(f.fileObj, f.maxFileSize)
+	if err != nil {
+		fmt.Printf("check size error err:%v\n", err)
+		return
+	}
+	if isBig {
+		f.fileObj = f.splitFile(f.fileObj)
+	}
+	select {
+	case logTmp := <-f.logChan:
+		_, _ = fmt.Fprintf(f.fileObj, "[%s] [%s] [%s:%s:%d] %v\n", logTmp.timestamp, parseLevel(f.Level), logTmp.funcName, logTmp.fileName, logTmp.line, logTmp.msg)
 		if f.Level >= ERROR {
 			if isBig {
 				f.errFileObj = f.splitFile(f.errFileObj)
 			}
-			_, _ = fmt.Fprintf(f.errFileObj, "[%s] [%s] [%s:%s:%d] %v\n", t, parseLevel(f.Level), funcName, fileName, lineNo, msg)
+			_, _ = fmt.Fprintf(f.errFileObj, "[%s] [%s] [%s:%s:%d] %v\n", logTmp.timestamp, parseLevel(f.Level), logTmp.funcName, logTmp.fileName, logTmp.line, logTmp.msg)
 		}
+	default: //取不到日志休息500毫秒
+		time.Sleep(time.Millisecond * 500)
+	}
+
+}
+
+func (f *FileLogger) RecordFileLog(printLevel LogLevel, msg ...interface{}) {
+	if f.Level >= printLevel {
+
+		funcName, fileName, lineNo := getInfo(2)
+
+		//先把日志发送到通道中
+		logTmp := &logMsg{
+			Level:     printLevel,
+			msg:       msg,
+			funcName:  funcName,
+			fileName:  fileName,
+			timestamp: time.Now().Format("2006-01-02 15:04:05"),
+			line:      lineNo,
+		}
+
+		select {
+		case f.logChan <- logTmp:
+		default: //保证业务代码顺畅执行
+		}
+
 	}
 }
 
